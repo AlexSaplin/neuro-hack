3c2a7a99c212fda6f687b7f07ec89000b0baf8f1
ashat1701 <ashat1701@github.com>
2019-06-30 05:47:39 +0300

    add login pages

--- site/server.py
+++ site/server.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from flask import render_template, sessions, redirect, url_for
+from flask import render_template, sessions, redirect, url_for, request
 from services.users.users_interface import UsersInterface
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("login.html")
+    if 'user_id' not in sessions:
+        return render_template("login.html")
+    else:
+        return redirect(url_for("list"))
 
 @app.route("/task/<id>")
 def task(id):
@@ -38,3 +41,19 @@ def completeTaskHandler(id):
     ui.remove_task_executor(task_id=int(id), user_id=user_id)
     return redirect(url_for('list'))
 
+@app.route('/login', methods=['POST'])
+def check():
+    if app.users_interface.check_user_data(request.form['u'], request.form['p']):
+        id = app.users_interface.check_user_data(request.form['u'], request.form['p'])
+        return  redirect(url_for('list'))
+    return render_template('login_failure.html')
+
+
+@app.route('/reg', methods=['POST'])
+def reg():
+    try:
+        id = app.users_interface.add_user(request.form['u'], request.form['p'])
+    except Exception as e:
+        return render_template('reg_failure.html')
+    sessions['user_id'] = id
+    return redirect(url_for('list'))
