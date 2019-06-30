52d8ff32e6c49d5eb8a4b21db12b5cc4f2a99638
molodec3 <chernov.andrei@phystech.edu>
2019-06-30 14:45:08 +0300

    kracivie graphiki

--- front/server.py
+++ front/server.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, session, redirect, url_for, request
+import random
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
+from flask import Flask, render_template, session, redirect, url_for, request, Markup
 from services.users.users_interface import UsersInterface
 
 app = Flask(__name__)
 app.secret_key = b'Ya.TvoyOtez'
+
+
 def start():
     app.run()
+
 
 @app.route("/")
 def hello():
@@ -12,6 +18,7 @@ def hello():
         return render_template("login.html")
     else:
         return redirect(url_for("listHandler"))
+
 
 @app.route("/task/<id>")
 def task(id):
@@ -22,7 +29,8 @@ def task(id):
         if task.id == int(id):
             current_task = task
     return render_template("task-page.html", name=current_task.name, id=current_task.id, description=current_task.description,
-                    author = ui.get_usermeta_by_id(current_task.author_id).name)
+                           author=ui.get_usermeta_by_id(current_task.author_id).name)
+
 
 @app.route("/doTask/<id>", methods=['POST'])
 def doTaskHandler(id):
@@ -37,6 +45,26 @@ def doTaskHandler(id):
     return render_template("doTask.html", name=current_task.name, id=id)
 
 
+@app.route("/givenTask/<id>", methods=['GET'])
+def givenTaskHandler():
+    ui = UsersInterface()
+    user_id = session['user_id']
+    ui.add_task_executor(task_id=int(id), user_id=user_id)
+    tasks = ui.get_tasks()
+    current_task = None
+    for task in tasks:
+        if task.id == int(id):
+            current_task = task
+    result = ui.fetch_task_results(current_task.id)
+    my_plot_div = plot([Scatter(x=result, y=[i for i in range(len(result))])], output_type='div')
+    # my_plot_div = plot([Scatter(y=[random.random() * 100 for i in range(1000)], x=[i for i in range(1000)])],
+    #                    output_type='div')
+    return render_template('givenTask.html',
+                           div_placeholder=Markup(my_plot_div)
+                           )
+    # return render_template('givenTask.html', name=current_task.name, id=id)
+
+
 @app.route("/completed/<id>", methods=["POST"])
 def completeTaskHandler(id):
     ui = UsersInterface()
@@ -44,13 +72,14 @@ def completeTaskHandler(id):
     ui.remove_task_executor(task_id=int(id), user_id=user_id)
     return redirect(url_for('listHandler'))
 
+
 @app.route('/login', methods=['POST'])
 def check():
     ui = UsersInterface()
     if ui.check_user_data(request.form['u'], request.form['p']):
         id = ui.check_user_data(request.form['u'], request.form['p'])
         session['user_id'] = id
-        return  redirect(url_for('listHandler'))
+        return redirect(url_for('listHandler'))
     return render_template('login_failure.html')
 
 
@@ -65,11 +94,13 @@ def reg():
     session['user_id'] = id
     return redirect(url_for('listHandler'))
 
+
 @app.route('/list')
 def listHandler():
     ui = UsersInterface()
     tasks = ui.get_tasks()
     return render_template('list.html', tasks=tasks)
+
 
 @app.route('/mylist')
 def mylistHandler():
@@ -78,10 +109,12 @@ def mylistHandler():
     tasks = ui.get_tasks(user_id=user_id)
     return render_template('mylist.html', tasks=tasks)
 
+
 @app.route('/logout', methods=['POST', 'GET'])
 def logoutHandler():
     del session['user_id']
     return render_template('login.html')
+
 
 @app.route('/reg', methods=['POST', 'GET'])
 def regHandler():
